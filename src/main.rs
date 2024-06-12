7d5b88dddc08536d41e5c596c8cdfd714066c674
David Sánchez <davidslt+git@pm.me>
2024-06-13 00:14:42 +0100

    feat: use 2 tasks, increase arena size

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,10 @@ use esp_hal::{
     gpio::{Gpio8, Gpio9, Io, Level, Output},
     peripherals::Peripherals,
     prelude::*,
-    rmt::Rmt,
+    rmt::{Channel, Rmt},
     system::SystemControl,
     timer::timg::TimerGroup,
+    Blocking,
 };
 use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
 use esp_println::logger::init_logger;
@@ -32,12 +33,41 @@ mod front_leds;
 // mod pixel_click; // A sort of framework for the board, not yet ready
 
 #[embassy_executor::task]
-async fn run(mut red: Output<'static, Gpio8>, mut blue: Output<'static, Gpio9>) {
+async fn front_leds(mut red: Output<'static, Gpio8>, mut blue: Output<'static, Gpio9>) {
     loop {
         info!("Playing with the leds concurrently!");
         red.toggle();
         blue.toggle();
         Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn led_panel(mut a_led: SmartLedsAdapter<Channel<Blocking, 0>, 865>) {
+    let mut color = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
+    let mut data;
+
+    loop {
+        info!("Iterating over the rainbow!");
+        // Iterate over the rainbow!
+        for hue in 0..=255 {
+            color.hue = hue;
+            // Convert from the HSV color space (where we can easily transition from one
+            // color to the other) to the RGB color space that we can then send to the LED
+            data = [hsv2rgb(color); 36];
+            // When sending to the LED, we do a gamma correction first (see smart_leds
+            // documentation for details) and then limit the brightness to 10 out of 255 so
+            // that the output it's not too bright.
+            a_led
+                .write(brightness(gamma(data.iter().cloned()), 15))
+                .unwrap();
+
+            Timer::after(Duration::from_millis(15)).await;
+        }
     }
 }
 
@@ -58,7 +88,6 @@ async fn main(spawner: Spawner) -> ! {
     // Init the front board LEDs
     let front_red = Output::new(io.pins.gpio8, Level::High);
     let front_blue = Output::new(io.pins.gpio9, Level::Low);
-    spawner.spawn(run(front_red, front_blue)).ok();
 
     // For the LED panel, initialize the RMT (Remote Control Transceiver)
     let rmt = Rmt::new(peripherals.RMT, 80.MHz(), &clocks, None).unwrap();
@@ -66,30 +95,13 @@ async fn main(spawner: Spawner) -> ! {
     // be used directly with all `smart_led` implementations
     // Our PixelClick has 36 LEDs
     let rmt_buffer = smartLedBuffer!(36);
-    let mut a_led = SmartLedsAdapter::new(rmt.channel0, io.pins.gpio5, rmt_buffer, &clocks);
+    let a_led = SmartLedsAdapter::new(rmt.channel0, io.pins.gpio5, rmt_buffer, &clocks);
 
-    let mut color = Hsv {
-        hue: 0,
-        sat: 255,
-        val: 255,
-    };
-    let mut data;
+    // Spawn the two tasks!
+    spawner.spawn(front_leds(front_red, front_blue)).ok();
+    spawner.spawn(led_panel(a_led)).ok();
 
     loop {
-        // Iterate over the rainbow!
-        for hue in 0..=255 {
-            color.hue = hue;
-            // Convert from the HSV color space (where we can easily transition from one
-            // color to the other) to the RGB color space that we can then send to the LED
-            data = [hsv2rgb(color); 36];
-            // When sending to the LED, we do a gamma correction first (see smart_leds
-            // documentation for details) and then limit the brightness to 10 out of 255 so
-            // that the output it's not too bright.
-            a_led
-                .write(brightness(gamma(data.iter().cloned()), 15))
-                .unwrap();
-
-            Timer::after(Duration::from_millis(15)).await;
-        }
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
